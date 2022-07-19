b2f3ce07d57a0cc121c47b9d55f61d4e91bfccd4
Dale <xiangkeguo@gmail.com>
2022-07-19 21:34:54 +0800

    ok

--- app/Observers/WechatBotContactObserver.php
+++ app/Observers/WechatBotContactObserver.php
@@ -2,12 +2,7 @@
 
 namespace App\Observers;
 
-use App\Models\WechatContent;
-use App\Models\WechatMessage;
-use Illuminate\Support\Facades\Log;
-use Spatie\WebhookServer\WebhookCall;
-use Illuminate\Support\Facades\Cache;
-use App\Events\WechatMessageCreated;
+use App\Models\WechatBotContact;
 
 class WechatBotContactObserver
 {
