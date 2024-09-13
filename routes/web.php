8863433a9f250d415a42542cb2750c3fa75a375a
me@mbp.m3 <TWR-****-****-****-7F47>
2024-09-12 21:47:37 -0700

    Febc Resend

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ use App\Http\Livewire\WechatContent;
 use App\Http\Livewire\WechatBotContact;
 use App\Http\Livewire\WechatBotContactTag;
 use App\Http\Livewire\WechatAutoReply;
+use Illuminate\Support\Facades\Log;
+use App\Jobs\reSendQueue;
+use App\Models\XbotSubscription;
+use App\Models\WechatBot;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -37,4 +42,40 @@ Route::group([
         Route::get('/contact', WechatBotContact::class)->name('contact');
         Route::get('/autoreply', WechatAutoReply::class)->name('autoreply');
         Route::get('/tags', WechatBotContactTag::class)->name('tags');
+});
+ // App\Models\WechatBot [281,"XbotIsLive 程序崩溃时,已下线！","wxid_7nof1pauaqyd22",10]
+
+Route::group([
+    'middleware' => ['auth:sanctum', 'verified'],
+    ], function () {
+        Route::get('/reSend/{keyword}', function ($keyword) {
+            $botOwnerId = auth()->user()->currentTeam->owner->id;
+            $wechatBot = WechatBot::where('user_id', $botOwnerId)->firstOrFail();
+            $xbotSubscriptions = XbotSubscription::where('wechat_bot_id', $wechatBot->id)->where('keyword', $keyword)->get();
+            
+            // clear cache then reSend!
+            $cacheKey = "resources.{$keyword}";
+            Cache::forget($cacheKey);
+            foreach ($xbotSubscriptions as $xbotSubscription) {
+                Log::debug('reSend', [$wechatBot->id, $keyword, $xbotSubscription->id]);
+                reSendQueue::dispatch($xbotSubscription);
+            }
+            return ['已加入发送队列','发送中，请到手机微信上查看'];
+        });
+
+        Route::get('/reSend/all', function () {
+            $botOwnerId = auth()->user()->currentTeam->owner->id;
+            $wechatBot = WechatBot::where('user_id', $botOwnerId)->firstOrFail();
+            $xbotSubscriptions = XbotSubscription::where('wechat_bot_id', $wechatBot->id)->get();
+            foreach ($xbotSubscriptions as $xbotSubscription) {
+                $keyword = $xbotSubscription->keyword;
+                // clear cache then reSend!
+                $cacheKey = "resources.{$keyword}";
+                Cache::forget($cacheKey);
+                Log::debug('reSendAll', [$wechatBot->id, $keyword, $xbotSubscription->id]);
+                reSendQueue::dispatch($xbotSubscription);
+            }
+            return ['已全部加入发送队列','发送中，请到手机上查看'];
+        });
+
 });
